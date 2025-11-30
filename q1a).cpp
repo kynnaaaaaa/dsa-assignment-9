2a82bfdb95bfe8a069c6f6b5c165881226d82d60
kynnaaaaaa <kynasood2006@gmail.com>
2025-11-30 17:57:00 +0530

    q1a).cpp

---
+++ q1a).cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+void BFS(int start, vector<vector<int>>& adj, int n) {
+    vector<bool> visited(n, false);
+    queue<int> q;
+
+    visited[start] = true;
+    q.push(start);
+
+    cout << "BFS: ";
+
+    while (!q.empty()) {
+        int u = q.front();
+        q.pop();
+        cout << u << " ";
+
+        for (int v : adj[u]) {
+            if (!visited[v]) {
+                visited[v] = true;
+                q.push(v);
+            }
+        }
+    }
+}
+
+int main() {
+    int n = 5;
+    vector<vector<int>> adj(n);
+
+    // Simple graph
+    adj[0] = {1, 2};
+    adj[1] = {0, 3};
+    adj[2] = {0, 4};
+    adj[3] = {1};
+    adj[4] = {2};
+
+    BFS(0, adj, n);
+    return 0;
+}
